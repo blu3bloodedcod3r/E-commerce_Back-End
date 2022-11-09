60cdf008834d26ef9f169bfdadab125e49fa7b43
Crystal Aguilar <crystal_0688@yahoo.com>
2022-11-09 13:19:55 -0800

    tag-routes added

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,30 +3,42 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
   try {
-
+    const productData = await Product.findAll({
+      include: [{model: Product}, {model: ProductTag}, {model: category}]
+    });
+    res.status(200).json(productData)
   } catch (err) {
     res.status(500).json(err)
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
   try {
+    const productID = await Product.findById(req.params.id, {
+      include: [{model: ProductTag}, {model: Product}]
+    });
 
+    if (!productID) {
+      res.status(404).json({ message: 'No product found with that id!' });
+      return;
+    }
+
+    res.status(200).json(productID)
   } catch (err) {
     res.status(500).json(err)
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
   try {
-
+    const newTag = await Tag.create()
   } catch (err) {
     res.status(500).json(err)
   }
